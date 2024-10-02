a988a19492bd4ae920b2b9b78312a062c55ae7cb
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2024-10-02 07:59:10 +0200

    Update ORGANISATIE (aantal) functies per partner per afdeling.sql

--- ORGANISATIE (aantal) functies per partner per afdeling.sql
+++ ORGANISATIE (aantal) functies per partner per afdeling.sql
@@ -3,12 +3,14 @@
 
 ------------------------
 --detail functies per ID
-SELECT of.partner_id afd_id, a1.name afdeling, of.person_id partner_id, p.first_name || ' ' || p.last_name partner, of.name 
+SELECT /*of.partner_id afd_id, a1.name afdeling,*/ of.person_id partner_id, p.first_name || ' ' || p.last_name partner, of.id functie_id, of.name functie_naam
+--SELECT DISTINCT p.id
 FROM res_organisation_function of 
 	JOIN res_partner p ON p.id = of.person_id
 	JOIN res_partner a1 ON a1.id = of.partner_id
-WHERE /*of.partner_id = 248646 AND*/ of.person_id = 20065
-ORDER BY p.id, a1.id
+--WHERE /*of.partner_id = 248646 AND*/ of.person_id = 20065
+WHERE p.active = false
+ORDER BY p.id--, a1.id
 
 
 ------------------------
